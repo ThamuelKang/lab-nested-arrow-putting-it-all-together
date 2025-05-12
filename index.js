f02868271ebe1f971b909f1e758a516235d24e83
Samuel Kang <samuel.june.kang@gmail.com>
2025-05-11 21:21:51 -0700

    fixed conditionals

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function createLoginTracker(userInfo) {
 
     if (passwordAttempt === userInfo.password && attemptCount <= 3) {
       return 'Login successful'
-    } else if (attemptCount < 3) {
+    } else if (attemptCount <= 3) {
       return `Attempt ${attemptCount}: Login failed`
-    } else if (attemptCount === 3) {
+    } else if (attemptCount > 3) {
       return `Account locked due to too many failed login attempts`
     }
   }
