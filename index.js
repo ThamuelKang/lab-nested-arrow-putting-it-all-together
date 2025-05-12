e370f92ecd0d0a33e1175b4b26f4fe29d26e53c3
Samuel Kang <samuel.june.kang@gmail.com>
2025-05-11 21:01:23 -0700

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+function createLoginTracker(userInfo) {
+  userInfo.name
+  userInfo.password
+
+  let attemptCount = 0
+
+  passwordCheck = (passwordAttempt) => {
+    attemptCount++
+
+    if (passwordAttempt === userInfo.password && attemptCount <= 3) {
+      return console.log('Login successful')
+    } else if (attemptCount < 3) {
+      return console.log(`Attempt ${attemptCount}: Login failed `)
+    } else {
+      return console.log(`Account locked due to too many failed login attempts`)
+    }
+  }
+}
+
+
 
 
 
